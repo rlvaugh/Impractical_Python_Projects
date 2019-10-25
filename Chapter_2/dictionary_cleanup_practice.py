1af4a362906453ffe6da532fc3cc86655d1479e9
rlvaugh <31315095+rlvaugh@users.noreply.github.com>
2019-10-25 11:08:55 -0500

    Update dictionary_cleanup_practice.py
    Corrected bug if two single letter words other than "a" and "I" occur consecutively in input list.

--- Chapter_2/dictionary_cleanup_practice.py
+++ Chapter_2/dictionary_cleanup_practice.py
@@ -1,11 +1,16 @@
 """Remove single-letter words from list if not 'a' or 'i'."""
-word_list = ['a', 'nurses', 'i', 'stack', 'b', 'cats', 'c']
+word_list = ['a', 'nurses', 'i', 'stack', 'b', 'c', 'cat']
+word_list_clean = []
 
 permissible = ('a', 'i')
 
 # remove single-letter words if not "a" or "I"
 for word in word_list:
-    if len(word) == 1 and word not in permissible:
-        word_list.remove(word)
+    if len(word) > 1:
+        word_list_clean.append(word)
+    elif len(word) == 1 and word in permissible:
+        word_list_clean.append(word)
+    else:
+        continue
 
-print("{}".format(word_list))
+print("{}".format(word_list_clean))

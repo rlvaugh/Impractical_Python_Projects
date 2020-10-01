f5c26de555e5beec116610d7714a2b67fa7c9041
prabin123-proo <71684870+prabin123-proo@users.noreply.github.com>
2020-10-01 19:35:31 +0545

    common mistakes that you need to improve
    try to write your variable in small letters and in such way that it can be meaningful to that programme that you made. hope you understand

--- Chapter_1/ETAOIN_practice.py
+++ Chapter_1/ETAOIN_practice.py
@@ -3,20 +3,20 @@ import sys
 import pprint
 from collections import defaultdict
 
-# Note: text should be a short phrase for bars to fit in IDLE window
-text = 'Like the castle in its corner in a medieval game, I foresee terrible \
+# Note: text should be a short phrase for bars to fit in Idel window
+random_text = 'Like the castle in its corner in a medieval game, I foresee terrible \
 trouble and I stay here just the same.'
 
-ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+random_letters = 'abcdefghijklmnopqrstuvwxyz'
 
 # defaultdict module lets you build dictionary keys on the fly!
 mapped = defaultdict(list)
-for character in text:
-    character = character.lower()
-    if character in ALPHABET:
-        mapped[character].append(character)
+for character in random_text:
+    new_character = character.lower()
+    if character in random_letters:
+        mapped[new_character].append(character)
 
-# pprint lets you print stacked output
+# pprint lets you to print the stacked output of this program
 print("\nYou may need to stretch console window if text wrapping occurs.\n")
 print("text = ", end='')
 print("{}\n".format(text), file=sys.stderr)

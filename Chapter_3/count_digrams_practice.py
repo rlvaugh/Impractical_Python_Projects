5c1bf1e2af26600769fc52ec2b66bf9e79470693
rlvaugh <31315095+rlvaugh@users.noreply.github.com>
2021-07-01 17:58:21 -0500

    Update count_digrams_practice.py

--- Chapter_3/count_digrams_practice.py
+++ Chapter_3/count_digrams_practice.py
@@ -1,36 +1,39 @@
-"""Generate letter pairs in Voldemort & find their frequency in a dictionary.
+"""Generate letter pairs & find their frequency in dictionary file.
 
 Requires load_dictionary.py module to load an English dictionary file.
 
 """
+
 import re
 from collections import defaultdict
-from itertools import permutations
 import load_dictionary
 
-word_list = load_dictionary.load('2of4brif.txt')
+word_list = load_dictionary.load('2of4brif.txt', 'r')
 
-name = 'Voldemort'  #(tmvoordle)
-name = name.lower()
+name = 'voldemort'
 
 # generate unique letter pairs from name
 digrams = set()
-perms = {''.join(i) for i in permutations(name)}
-for perm in perms:
-    for i in range(0, len(perm) - 1):
-        digrams.add(perm[i] + perm[i + 1])
-print(*digrams, sep='\n')
-print("\nNumber of digrams = {}\n".format(len(digrams)))
+for letter in name:
+    for i in range(0, len(name)):
+        pair = letter + name[i]
+        digrams.add(pair)
+print(*sorted(digrams), sep='\n')
+print()
+print("Number of digrams = {}".format(len(digrams)))
+print()
 
 # use regular expressions to find repeating digrams in a word
-mapped = defaultdict(int)
+# mark each occurrence of digrams with dictionary value = 'x'
+# count number of 'x' per digram
+mapped = defaultdict(list)
 for word in word_list:
-    word = word.lower()
     for digram in digrams:
         for m in re.finditer(digram, word):
-            mapped[digram] += 1
+            mapped[digram].append('x')
 
 print("digram frequency count:")
 count = 0
-for k in mapped:
-    print("{} {}".format(k, mapped[k]))
+for i in sorted(mapped):
+    print("{} {}".format(i, len(mapped[i])))
+
